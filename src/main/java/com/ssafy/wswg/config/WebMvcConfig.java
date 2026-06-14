8803929c4804b60b7cd3a8298b3d7f65f2096710
uijae <kuda1390@dgu.ac.kr>
2026-06-14 17:56:54 +0900

    add : interceptor에 @LoginUserId 어노테이션 로직 추가

--- src/main/java/com/ssafy/wswg/config/WebMvcConfig.java
+++ src/main/java/com/ssafy/wswg/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package com.ssafy.wswg.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ssafy.wswg.interceptor.LoggingInterceptor;
+import com.ssafy.wswg.security.LoginUserIdArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +17,15 @@ import lombok.RequiredArgsConstructor;
 public class WebMvcConfig implements WebMvcConfigurer {
 	
 	private final LoggingInterceptor loggingInterceptor;
+	private final LoginUserIdArgumentResolver loginUserIdArgumentResolver;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loggingInterceptor);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginUserIdArgumentResolver);
 	}
 }
